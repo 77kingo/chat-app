010a2c7a7f2c08bda685e21e0a4b11c515e3819e
77kingo <singhsaurav8899@gmail.com>
2024-12-06 11:52:16 +0545

    Add public directory with client-side chat interface

--- server.js
+++ server.js
@@ -6,26 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Chat App!');
-});
+app.use(express.static('public'));
 
-io.on('connection', (socket) => {
-    console.log('New client connected');
-
-    socket.on('error', (err) => {
-        console.error('Socket error:', err);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 io.on('connection', (socket) => {
     console.log('New client connected');
     
@@ -36,4 +18,9 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
